dbb922b3345cd51a850b519b934e241d76b690fa
Gabriel Rezende <g@brielrezen.de>
2021-08-03 12:25:19 -0300

    input: System.in p2

--- Hello/src/MeuPrograminha.java
+++ Hello/src/MeuPrograminha.java
@@ -1,9 +1,14 @@
+import java.util.Scanner; // Sem importar daria pra puxar ainda usando: java.util.Scanner scanner = new java.util.Scanner(System.in);
 
 public class MeuPrograminha {
 
 	public static void main(String[] args) {
-		System.out.println("Olá!");
-
+		System.out.println("Qual é o seu nome? "); // Da pra aceitar um argumento na execução: println("Olá! " + args[0] + ".");
+		
+		Scanner escaner = new Scanner(System.in); // criando a variável "scanner": type, indentifier, novo // System.in indica que é do console que tá pegando
+		String nome = escaner.nextLine();
+		
+		System.out.println("Olá "+ nome + ".");  // Atalho: digitar "sysout", segurar "control" e apertar "espaço"
 	}
 
 }
